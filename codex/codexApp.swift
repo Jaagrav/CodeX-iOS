586454b6fe99c631e0a5b1584913d602728e9cbe
Jaagrav <contact@jaagrav.in>
2023-01-22 15:56:51 +0530

    Initial Commit

---
+++ codex/codexApp.swift
@@ -0,0 +1,17 @@
+//
+//  codexApp.swift
+//  codex
+//
+//  Created by Jaagrav Seal on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct codexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
